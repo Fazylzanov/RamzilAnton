10697307258ca96a6d5320fec6bd04a5c2e3fbe4
Fazylzanov <ramzilfazylzanov@icloud.com>
2025-01-13 10:42:08 +0300

    ff

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace App_Arhipov_
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace App_Arhipov_
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
